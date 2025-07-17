bafd615753aa4cb1d749c384059f8023f005790a
Roger Lopes <rogersouzasilva@hotmail.com>
2025-07-17 18:57:58 -0300

    Subindo alteração heranca

--- Heranca/heranca/Animal.java
+++ Heranca/heranca/Animal.java
@@ -1,5 +1,3 @@
-package Heranca.heranca;
-
 public class Animal {
 
 }
--- Heranca/heranca/Program.java
+++ Heranca/heranca/Program.java
@@ -1,5 +1,3 @@
-package Heranca.heranca;
-
 public class Program {
     public static void main(String[] args) {
         Animal meuAnimal = new Animal();
