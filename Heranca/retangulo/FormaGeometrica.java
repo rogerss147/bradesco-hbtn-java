d1d2ac9ddeb3cfd892f91e1b28bb3877bc7f527e
Roger Lopes <rogersouzasilva@hotmail.com>
2025-07-17 21:10:00 -0300

    subindo alteração do retangulo

---
+++ Heranca/retangulo/FormaGeometrica.java
@@ -0,0 +1,7 @@
+package Heranca.retangulo;
+
+public class FormaGeometrica {
+    public double area() throws Exception {
+        throw new java.lang.UnsupportedOperationException("Funcao area nao implementada");
+    }
+}
