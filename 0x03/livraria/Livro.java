c2233a06b82f91da6e819ef1e9f1fe1a9bdc63fd
Roger Lopes <rogersouzasilva@hotmail.com>
2025-07-25 14:54:03 -0300

    subindo exercicio livraria

---
+++ 0x03/livraria/Livro.java
@@ -0,0 +1,47 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
+
+public class Livro {
+    public String titulo;
+    public String autor;
+    public double preco;
+
+    public Livro(String titulo, String autor, double preco) throws LivroInvalidoException, AutorInvalidoException {
+        this.setTitulo(titulo);
+        this.setAutor(autor);
+        this.setPreco(preco);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) throws LivroInvalidoException {
+        if (titulo.length() >= 3)
+            this.titulo = titulo;
+        else
+            throw new LivroInvalidoException("Titulo de livro invalido");
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) throws AutorInvalidoException {
+        if (autor.contains(" "))
+            this.autor = autor;
+        else
+            throw new AutorInvalidoException("Nome de autor invalido");
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) throws LivroInvalidoException {
+        if (preco > 0)
+            this.preco = preco;
+        else
+            throw new LivroInvalidoException("Preco de livro invalido");
+    }
+}
---
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -0,0 +1,15 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
+
+public class LivroEdicaoOuro extends Livro {
+
+    public LivroEdicaoOuro(String titulo, String autor, double preco)
+            throws AutorInvalidoException, LivroInvalidoException {
+        super(titulo, autor, preco);
+    }
+
+    @Override
+    public double getPreco() {
+        return preco += (preco * 0.30);
+    }
+}
