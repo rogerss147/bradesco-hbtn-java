fba4269092e296a1e12931c847fdd045c6e4260f
Roger Lopes <rogersouzasilva@hotmail.com>
2025-08-26 20:07:14 -0300

    incluindo comparação com .equals

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -3,10 +3,10 @@ public class PessoasArray {
     private String[] nomes;
 
     public PessoasArray() {
-        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
-                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
-                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
-                "Tais","Umberto","Vanessa","Xavier"};
+        nomes = new String[] { "Amanda", "Beatriz", "Carlos", "Daniela", "Eduardo",
+                "Fabio", "Gustavo", "Hingrid", "Isabela", "Joao", "Leise", "Maria",
+                "Norberto", "Otavio", "Paulo", "Quirino", "Renata", "Sabata",
+                "Tais", "Umberto", "Vanessa", "Xavier" };
     }
 
     public String[] getNomes() {
@@ -18,17 +18,17 @@ public class PessoasArray {
     }
 
     // implementar o método de buscaLinear
-    public void buscaLinear(String nome){
+    public void buscaLinear(String nome) {
         boolean encontrado = false;
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
-        for(int i = 0; i < nomes.length; i++) {
+        for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
-            if(nomes[i] == nome) {
+            if (nomes[i].equalsIgnoreCase(nome)) {
                 System.out.println("Nome pesquisado é " + nomes[i] + " que está na posição " + i);
                 encontrado = true;
             }
         }
-        if(!encontrado){
+        if (!encontrado) {
             throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
         }
 
