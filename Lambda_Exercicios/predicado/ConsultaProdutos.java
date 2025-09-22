92a6d201621abb42f8f4016ba9c12778acedf14a
Roger Lopes <rogersouzasilva@hotmail.com>
2025-09-22 20:34:41 -0300

    subindo exercicios.

---
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ConsultaProdutos {
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterio) {
+        return produtos.stream()
+                .filter(criterio::test)
+                .toList();
+    }
+}
